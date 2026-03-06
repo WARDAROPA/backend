c0750e1c9c0d09cfeac8a812076bf9dae2f7fe02
Gorka Hernandez Villalon <131766898+Gorkah@users.noreply.github.com>
2026-03-06 13:06:54 +0100

    #18 n8n json texto a imagen ia sql, + production webhoook n8n

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 const app = ExpressWs(express()).app;
 const PORT = 3000;
-const DEFAULT_N8N_POST_DESCRIPTION_WEBHOOK_URL = 'https://sql3.srv869945.hstgr.cloud/webhook-test/3717090c-f984-499d-b1b7-4820eb32970b';
+const DEFAULT_N8N_POST_DESCRIPTION_WEBHOOK_URL = 'https://sql3.srv869945.hstgr.cloud/webhook/3717090c-f984-499d-b1b7-4820eb32970b';
 
 app.use((req, res, next) => {
   const allowedOrigins = ['http://localhost:4200', 'https://4.233.184.106', 'http://4.233.184.106', 'https://wardaropa.github.io'];
